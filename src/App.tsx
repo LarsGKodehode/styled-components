f76b86dc2087f1bf019ad3eeb8a03f06b22e0ab3
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-08-15 16:23:09 +0200

    first test for styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 // CSS
-import styles from './App.module.css';
+import styled from 'styled-components';
+import './App.css';
 
 // Components
+import Navbar from './assets/components/Navbar/Navbar'
+
+const StyledSection = styled.section`
+margin: 0 2em;
+padding: 2em;
+background-color: #6c30bfd8;
+`
+
+const StyledH1 = styled.h1`
+color: black;
+`
+
+const StyledParagraph = styled.p`
+color: black;
+`
 
 function App() {
   return (
-    <main className={styles['App']}>
+    <main className='App'>
+      <Navbar />
+
+      <StyledSection>
+
+        <StyledH1>My Styled Test Text</StyledH1>
+
+        <StyledParagraph>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda dignissimos provident sint quae, nihil voluptatum labore itaque odio facere porro, deleniti ex. Obcaecati natus, illum hic iste aliquam repellat? Mollitia.</StyledParagraph>
+
+      </StyledSection>
     </main>
   );
 };
