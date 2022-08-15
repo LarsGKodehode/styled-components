a3b21c442adca6c9542f92575ec3fa781170a873
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-08-15 11:50:26 +0200

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,11 @@
 // CSS
 import styles from './App.module.css';
 
-// Assets
-import logo from '/logo.svg';
-
 // Components
 
 function App() {
   return (
     <main className={styles['App']}>
-      <img src={logo} className="logo" alt="logo" />
     </main>
   );
 };
