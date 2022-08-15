09da3ff41818e0503fca6c709ceada2d268051ab
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-08-15 15:09:29 +0200

    removed react-router-dom
    project is too simple to us it

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
 // 3rd Parties Libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom'
 
 // CSS
 import './index.css';
@@ -33,12 +28,6 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' element={<App />} />
-
-          <Route path='*' element={<App />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
