a35972e8987989e6e4c7f036312e367266e7c8ce
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-08-15 11:14:52 +0200

    touched up on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
+// 3rd Parties Libraries
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+// CSS
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+/**
+ * DEVELOPMENT
+ * clears console of error messages between hotswaps
+ * @link https://github.com/vitejs/vite/discussions/3143
+ */ 
+ if (import.meta.hot) {
+  import.meta.hot.on(
+    "vite:beforeUpdate",
+    () => console.clear()
+  );
+}
+// DEVELOPMENT
+
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
